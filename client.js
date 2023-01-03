b4e405b876b896fb439198a56c0bf43593f185c8
Chris <chris@freeperiod.co.uk>
2023-01-03 20:16:14 +0000

    updated buffer

--- client.js
+++ client.js
@@ -2,7 +2,7 @@ const dgram = require('dgram');
 
 const HOST = '0.0.0.0';
 const PORT = 30001;
-const message = new Buffer('My KungFu is Good!');
+const message = new Buffer.from('My KungFu is Good!');
 const client = dgram.createSocket('udp4');
 
 client.send(message, 0, message.length, PORT, HOST, function(err, bytes) {
